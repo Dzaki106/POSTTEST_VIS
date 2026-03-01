4febf5e62c98dbc41bdcb129f1446cce10e8b241
Dzaki106 <Zmubarak746@gmail.com>
2026-03-01 23:40:14 +0800

    Add files via upload

---
+++ POSTTEST_1/2409106106_DzakiMubarak_C2'24/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(271, 185)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "TAMBAH"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(398, 185)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 1
+        Button2.Text = "RESET"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(141, 141)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(115, 25)
+        Label1.TabIndex = 2
+        Label1.Text = "IP SEMESTER"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(141, 239)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(125, 25)
+        Label2.TabIndex = 3
+        Label2.Text = "IP KUMULATIF"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(281, 295)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(56, 25)
+        Label3.TabIndex = 4
+        Label3.Text = "NILAI"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(271, 141)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(271, 236)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "KALKULATOR IP"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+
+End Class
---
+++ POSTTEST_1/2409106106_DzakiMubarak_C2'24/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+    Private daftarIPSemester As New List(Of Double)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox2.ReadOnly = True
+        TextBox2.Text = "0.00"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If String.IsNullOrWhiteSpace(TextBox1.Text) Then
+            TextBox1.Focus()
+            Return
+        End If
+
+        Dim ipSemester As Double
+        If Not Double.TryParse(TextBox1.Text, ipSemester) Then
+            TextBox1.Clear()
+            TextBox1.Focus()
+            Return
+        End If
+
+        If ipSemester < 0 Or ipSemester > 4 Then
+            TextBox1.Clear()
+            TextBox1.Focus()
+            Return
+        End If
+
+        daftarIPSemester.Add(ipSemester)
+
+        Dim total As Double = 0
+        For i As Integer = 0 To daftarIPSemester.Count - 1
+            total = total + daftarIPSemester(i)
+        Next
+
+        Dim ipKumulatif As Double = total / daftarIPSemester.Count
+
+        TextBox2.Text = ipKumulatif.ToString("0.00")
+
+        If ipKumulatif >= 2.0 And ipKumulatif <= 2.75 Then
+            Label3.Text = "Cukup"
+        ElseIf ipKumulatif >= 2.76 And ipKumulatif <= 3.0 Then
+            Label3.Text = "Memuaskan"
+        ElseIf ipKumulatif >= 3.01 Then
+            Label3.Text = "Sangat Memuaskan"
+        Else
+            Label3.Text = "-"
+        End If
+
+        TextBox1.Clear()
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        daftarIPSemester.Clear()
+        TextBox1.Clear()
+        TextBox2.Text = "0.00"
+        Label3.Text = ""
+        TextBox1.Focus()
+    End Sub
+End Class
